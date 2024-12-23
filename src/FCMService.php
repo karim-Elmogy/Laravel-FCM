563051b5ae6cd428aa839a4a6567a435a314c92f
Karim Elmogy <kimo72636@gmail.com>
2024-12-23 23:36:51 +0200

    GH

--- src/FCMService.php
+++ src/FCMService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace laravel\fcm;
+namespace firebase\fcm;
 
 use Google\Client as GoogleClient;
 use Illuminate\Support\Facades\Http;
--- src/FCMServiceProvider.php
+++ src/FCMServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace laravel\fcm;
+namespace firebase\fcm;
 
 
 
