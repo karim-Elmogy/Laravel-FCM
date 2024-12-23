cca3dea6e39a7d8c5ef5e7c155d03f58f209c00f
Karim Elmogy <kimo72636@gmail.com>
2024-12-23 23:22:39 +0200

    GH

---
+++ src/FCMServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace laravel\fcm;
+
+
+
+
+use Illuminate\Support\ServiceProvider;
+
+
+class FCMServiceProvider extends ServiceProvider{
+
+    public function boot(){
+
+    }
+
+    public function register()
+    {
+
+    }
+}
